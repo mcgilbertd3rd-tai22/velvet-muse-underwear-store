c284c992841b4cac543964482f84d0a0f21b88ea
mcgilbertd3rd-tai22 <mcgilbertd3rd@gmail.com>
2026-06-25 11:33:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,17 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
+server: {
+  allowedHosts: [
+    "skywaydorm.onrender.com"
+  ]
+},
+
+preview: {
+  allowedHosts: [
+    "skywaydorm.onrender.com"
+  ]
+},
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
     server: { entry: "server" },
